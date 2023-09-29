43ce58bf93ee5851c11e28d1af63d1028f447e28
Guillaume Wells <wellsguillaume@gmail.com>
2023-09-29 16:35:51 +0200

    feat(main): i3status for Mib

--- Targets/Mib/custom.d/i3.bash
+++ Targets/Mib/custom.d/i3.bash
@@ -1,5 +1,8 @@
 #! /bin/bash
 
 rm -rf $HOME/.config/i3/config
+rm -rf $HOME/.config/i3status/config
 
 ln -s $ACTIVE_PATH/i3/config $HOME/.config/i3/config
+ln -s $ACTIVE_PATH/i3/i3status/config $HOME/.config/i3status/config
+
