c16e0c2ee520eacb51aa59f624117b77a85ae1d5
Guillaume Wells <wellsguillaume@gmail.com>
2023-10-10 17:21:09 +0200

    feat(main): autocommands for wrap

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@ require('user.plugins')
 require('user.options')
 require('user.keymaps')
 require('user.abbreviations')
+require('user.autocommands')
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
